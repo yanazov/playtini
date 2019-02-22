e282e6cd91bf6e5e7ad74ecc9c63c039679caab9
yanazov <yan_azov@mail.ru>
2019-02-22 18:46:56 +0200

    slider autoplay

--- js/ui.js
+++ js/ui.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
       infinite: true,
       speed: 300,
       slidesToShow: 1,
+      autoplaySpeed: 2000,
+      autoplay: true,
       adaptiveHeight: true
    });
  });
@@ -61,69 +63,3 @@ $(document).on('click', '.welcome__create', function(){
    $('.login').show();   
 });
 
-
-/*
-
-$(document).ready(function(){
-   $('.active-scroll-pane').jScrollPane({
-      contentWidth: '0px'
-   });
-});
-
-$(document).ready(function(){
-   $('.projects-scroll-pane').jScrollPane({
-      contentWidth: '0px'
-   });
-});
-$(document).on('click', '.menu .nav li', function(){
-   $('.menu .nav li').removeClass('active');
-   $(this).addClass('active');
-   
-});
-
-$(document).on('click', '.content-tab a', function(){
-   $('.content-tab a').removeClass('active');
-   $(this).addClass('active');
-   var index = $(this).index('.content-tab a');
-   $(".content-tab-item").removeClass('active');
-   $(".content-tab-item").eq(index).addClass('active');
-});
-$(document).on('click', '.blok-projects-item-open', function(){
-   if($(this).find('i').hasClass('fa-angle-down')){
-      $(this).find('i').removeClass('fa-angle-down');
-      $(this).find('i').addClass('fa-angle-up');
-      $(this).parent().parent().parent().find('.blok-projects-item-more.open').height($('.blok-projects-item-more-wrap').height());
-      setTimeout(function(){
-            $('.projects-scroll-pane').jScrollPane({
-            contentWidth: '0px'
-         })
-      }, 600);
-     
-      return 0;
-   }
-   if($(this).find('i').hasClass('fa-angle-up')){
-      $(this).find('i').addClass('fa-angle-down');
-      $(this).find('i').removeClass('fa-angle-up');
-      $(this).parent().parent().parent().find('.blok-projects-item-more.open').height(0);
-      setTimeout(function(){
-         $('.projects-scroll-pane').jScrollPane({
-         contentWidth: '0px'
-      })
-   }, 600);
-      return 0;return 0;
-   }
-   
-});
-$(document).on('click', '.select-custom a', function(){
-   
-   $(this).parent().find('.select-custom-ul').show();
-      
-  
-   
-});
-$(document).on('click', '.select-custom-ul li', function(){
-   $(this).parent().parent().find('a').text($(this).text());
-   $(this).parent().hide();   
-});
-
-*/
